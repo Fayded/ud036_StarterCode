d55793be71aad67c9ac01efc2d43dfe48955fea7
fayded <kevin.d.fay@gmail.com>
2017-10-04 15:33:08 -0400

    move Video out of media

--- media.py
+++ media.py
@@ -1,10 +1,10 @@
 import webbrowser
 import video
+        
 
-
-class Movie(Video):
+class Movie(video.Video):
     def __init__(self, title, duration, storyline, poster_image, trailer_youtube):
-        Video.__init__(self, title, duration, storyline)
+        video.Video.__init__(self, title, duration, storyline)
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
 
--- tv_show.py
+++
@@ -1,7 +0,0 @@
-import video
-
-class TvShow(video):
-    def __init__(self, tv_season, tv_episode, tv_station):
-        self.season = tv_season
-        self.episode = tv_episode
-        self.station = tv_station
